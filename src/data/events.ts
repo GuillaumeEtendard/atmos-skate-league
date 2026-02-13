bf70ac5fc83761e022be29968da164427e231655
Guillaume Etendard <guillaume.etendard@gmail.com>
2026-02-14 01:24:31 +0400

    Update events.ts

--- src/data/events.ts
+++ src/data/events.ts
@@ -14,10 +14,10 @@ export interface EventItem {
 
 export const EVENTS: EventItem[] = [
   { id: 'king-15-mars', date: 'DIMANCHE 15 MARS', time: '18H00 / 20H00', title: 'King of the Road', type: 'king', totalSpots: TOTAL_SPOTS, comingSoon: false },
-  { id: 'queen-28-mars', date: 'SAMEDI 28 MARS', time: '19h30 / 21h30', title: 'Queen of the Road', type: 'queen', totalSpots: TOTAL_SPOTS, comingSoon: false },
   { id: 'king-11-avril', date: 'SAMEDI 11 AVRIL', time: '19h30 / 21h30', title: 'King of the Road', type: 'king', totalSpots: TOTAL_SPOTS, comingSoon: true },
-  { id: 'queen-25-avril', date: 'SAMEDI 25 AVRIL', time: '19h30 / 21h30', title: 'Queen of the Road', type: 'queen', totalSpots: TOTAL_SPOTS, comingSoon: true },
   { id: 'electric-9-mai', date: 'SAMEDI 9 MAI', time: '19h30 / 21h30', title: 'Électrique', type: 'electric', totalSpots: TOTAL_SPOTS, comingSoon: false },
+  { id: 'queen-28-mars', date: 'SAMEDI 28 MARS', time: '19h30 / 21h30', title: 'Queen of the Road', type: 'queen', totalSpots: TOTAL_SPOTS, comingSoon: false },
+  { id: 'queen-25-avril', date: 'SAMEDI 25 AVRIL', time: '19h30 / 21h30', title: 'Queen of the Road', type: 'queen', totalSpots: TOTAL_SPOTS, comingSoon: true },
   { id: 'mixte-24-mai', date: 'DIMANCHE 24 MAI', time: '18H00 / 20H00', title: 'Mixte', type: 'mixte', totalSpots: TOTAL_SPOTS, comingSoon: true },
 ];
 
