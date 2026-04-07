c51a92849a3ca194d8b7e66112a48d9287e4a86f
Guillaume Etendard <guillaume.etendard@gmail.com>
2026-04-08 00:36:49 +0400

    update instagram reels

--- src/data/instagramReels.ts
+++ src/data/instagramReels.ts
@@ -5,46 +5,60 @@
  */
 export const INSTAGRAM_REELS = [
   {
-    permalink: 'https://www.instagram.com/atmos.gear/reel/DUtfvoeD2pi/',
-    username: 'atmos.gear',
-    videoUrl: '/videos/compressed/DUtfvoeD2pi.mp4',
+    permalink: 'https://www.instagram.com/atmosskateleague/reel/DWUFXDFjCNz/',
+    username: 'atmosskateleague',
+    videoUrl: '/videos/compressed/DWUFXDFjCNz_min.mp4',
     posterUrl:
-      'https://scontent-iad3-1.cdninstagram.com/v/t51.82787-15/629145506_18091491788073123_9162718065700338632_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=104&ccb=7-5&_nc_sid=18de74&oh=00_Afv_Tj4hyHRznKR37-TGFQXYmYk-WO3yoUdUW9DkTwlz9A&oe=69A42F26',
+      'https://instagram.fdxb5-1.fna.fbcdn.net/v/t51.82787-15/656952547_18095832887073123_8850573633366600388_n.jpg?stp=dst-jpg_e15_tt6&_nc_cat=102&ig_cache_key=Mzg2MDczNDM1NDY1ODE3MzgxMTE4MDk1ODMyODg0MDczMTIz.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjEwODB4MTkyMC5zZHIuQzMifQ%3D%3D&_nc_ohc=R-z0mvQbtUIQ7kNvwGTGFn5&_nc_oc=AdoutqHFGRrTOP-TYmC8GqirAKrrwLqeVqzFawEaG_4CQT_x58N4rXVy03XBl6i4MMU&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fdxb5-1.fna&_nc_gid=kb_C9FWq5HxRjtVr_s6EuQ&_nc_ss=7a32e&oh=00_Af0fBzhXlymeQacvpBrGEzW7gaVrEQnnbUXRHcbbLViuaw&oe=69DB2469',
   },
   {
-    permalink: 'https://www.instagram.com/deeps.rollerskate/reel/DVJUMjsjAk9/',
-    username: 'deeps.rollerskate',
+    permalink: 'https://www.instagram.com/atmosskateleague/reel/DWJOm6zjMxk/',
+    username: 'atmosskateleague',
+    videoUrl: '/videos/compressed/DWJOm6zjMxk_min.mp4',
+    posterUrl:
+      'https://instagram.fdxb5-1.fna.fbcdn.net/v/t51.82787-15/655026116_18095362808073123_8401486513891032395_n.jpg?stp=dst-jpg_e15_tt6&_nc_cat=101&ig_cache_key=Mzg1NzY3ODgwMjk1MjcwMzA3NjE4MDk1MzYyODA1MDczMTIz.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjMwMDB4NTMzMi5zZHIuQzMifQ%3D%3D&_nc_ohc=5ZJ7kJN2eM4Q7kNvwFCaoDN&_nc_oc=Adr4Rp8GipH-H49SUVPJLj70UlGhbUND8WP-zrMNMSUnjDYKoVIn9SJENCDwJnmYlQo&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fdxb5-1.fna&_nc_gid=kb_C9FWq5HxRjtVr_s6EuQ&_nc_ss=7a32e&oh=00_Af0rSUHIK_-5glpKDgQ6EBKJx-YUYm1kwdi2a2Lr9wlhnA&oe=69DB245C',
+  },
+  {
+    permalink: 'https://www.instagram.com/atmosskateleague/reel/DVbyO4bkxf_/',
+    username: 'atmosskateleague',
+    videoUrl: '/videos/compressed/DVbyO4bkxf__min.mp4',
+    posterUrl:
+      'https://instagram.fdxb5-1.fna.fbcdn.net/v/t51.82787-15/641440428_18093164867073123_1295197334030268834_n.jpg?stp=dst-jpg_e15_tt6&_nc_cat=101&ig_cache_key=Mzg0NDg4NzYzMTg4MTI0NjcxOTE4MDkzMTY0ODYxMDczMTIz.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjcyMHgxMjgwLnNkci5DMyJ9&_nc_ohc=_pVwucgGGz4Q7kNvwG4nxbv&_nc_oc=AdpTNTyh9zxqs9TjItejWZeS_1SLRvB229BYS7bCNobhrt3rFb6-ZD9iZsC17w3aFDE&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fdxb5-1.fna&_nc_gid=kb_C9FWq5HxRjtVr_s6EuQ&_nc_ss=7a32e&oh=00_Af1-RwohAAO1US5B4CsIxCO2Si5iiH1FT181O64uReDsfA&oe=69DB30B7',
+  },
+  {
+    permalink: 'https://www.instagram.com/atmosskateleague/reel/DVQn8KMAekq/',
+    username: 'atmosskateleague',
+    videoUrl: '/videos/compressed/DVQn8KMAekq_min.mp4',
+    posterUrl:
+      'https://instagram.fdxb2-1.fna.fbcdn.net/v/t51.82787-15/643027680_18092773619073123_7573319718986105683_n.jpg?stp=dst-jpg_e15_tt6&_nc_cat=107&ig_cache_key=Mzg0MTc0NjE0MDA2ODQ0MDM2MjE4MDkyNzczNjEzMDczMTIz.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjEwODB4MTkyMC5zZHIuQzMifQ%3D%3D&_nc_ohc=hO27tySd7Z4Q7kNvwFOgVsN&_nc_oc=AdrqUNDk37Es9qflgx7Jz8DASnazKnWvaMHRkyroWO67d3V4VXb5cD4lFsIgfKxJtrM&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fdxb2-1.fna&_nc_gid=kb_C9FWq5HxRjtVr_s6EuQ&_nc_ss=7a32e&oh=00_Af3AVIVLy5nxDRXBa5_hhu86yk5NOT3uwJii_srnTVSpMA&oe=69DB43A7',
+  },
+  {
+    permalink: 'https://www.instagram.com/atmosskateleague/reel/DVJUMjsjAk9/',
+    username: 'atmosskateleague',
     videoUrl: '/videos/compressed/DVJUMjsjAk9_min.mp4',
     posterUrl:
-      'https://scontent-iad3-2.cdninstagram.com/v/t51.71878-15/626264676_906745648505004_8860500759557334457_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=111&ccb=7-5&_nc_sid=18de74&oh=00_AfsQrlhhdGR9t6oWSnMxwv_mLKCjs3Q9s-da9P4PkVxRcw&oe=69A428BB',
+      'https://instagram.fdxb2-1.fna.fbcdn.net/v/t51.82787-15/642413889_17935366269171181_5181422053171392947_n.jpg?stp=dst-jpg_e15_tt6&_nc_cat=110&ig_cache_key=MzgzOTY4ODk4MTIwMjM0MDE1NzE3OTM1MzY2MjYzMTcxMTgx.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjEwODB4MTkyMC5zZHIuQzMifQ%3D%3D&_nc_ohc=08cwln_lhqQQ7kNvwHmNC06&_nc_oc=AdrN62Pl08ySaJ9gCFHcRE977CqRuHksND5BywY6iux2wm2U3w57aGaPtPkGtLyQ3YM&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fdxb2-1.fna&_nc_gid=kb_C9FWq5HxRjtVr_s6EuQ&_nc_ss=7a32e&oh=00_Af1zarKzE2tRqqVz1lqexNYtlzPqSGVHSutdzItqQXOs8A&oe=69DB256E',
   },
   {
-    permalink: 'https://www.instagram.com/atmos.gear/reel/DVDiYucE7rA/',
-    username: 'atmos.gear',
-    videoUrl: '/videos/compressed/DVDiYucE7rA_min.mp4',
-    posterUrl:
-      'https://scontent-iad3-1.cdninstagram.com/v/t51.82787-15/639681216_18092303480073123_1907769393020219026_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=109&ccb=7-5&_nc_sid=18de74&oh=00_AfvODkIxCnJx4T_QRoiKq4mdnhauOAl-oLceoes3FBzNvw&oe=69A40A64',
-  },
-  {
-    permalink: 'https://www.instagram.com/atmos.gear/reel/DU8rKxBD9T0/',
-    username: 'atmos.gear',
+    permalink: 'https://www.instagram.com/atmosskateleague/reel/DU8rKxBD9T0/',
+    username: 'atmosskateleague',
     videoUrl: '/videos/compressed/DU8rKxBD9T0_min.mp4',
     posterUrl:
-      'https://scontent-iad3-1.cdninstagram.com/v/t51.82787-15/640261713_18092149520073123_7544430098461504160_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=101&ccb=7-5&_nc_sid=18de74&oh=00_AfsepLnZqivuRi1VhgF57nUPLcwhV8Gt6L7QuFZBODQJ7A&oe=69A3FAB1',
+      'https://instagram.fdxb5-1.fna.fbcdn.net/v/t51.82787-15/640261713_18092149520073123_7544430098461504160_n.jpg?stp=dst-jpg_e15_tt6&_nc_cat=101&ig_cache_key=MzgzNjEzMDgzODQzODcyODk0ODE4MDkyMTQ5NTE0MDczMTIz.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjEwODB4MTkyMC5zZHIuQzMifQ%3D%3D&_nc_ohc=M7GVjkcHm0sQ7kNvwF9cOoQ&_nc_oc=Adp-nM2wNu1pGKs2Lx2pOdWlHCa0e1ieb2CE97bTTo_DDEmtPKHLriTdW0qHw44dZcs&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fdxb5-1.fna&_nc_gid=kb_C9FWq5HxRjtVr_s6EuQ&_nc_ss=7a32e&oh=00_Af2SnqKkfdsyFOdCM175zO3lgQPNfl1sga_5pRWo_yqh4g&oe=69DB2171',
   },
   {
-    permalink: 'https://www.instagram.com/atmos.gear/reel/DU3hjmfApa4/',
-    username: 'atmos.gear',
+    permalink: 'https://www.instagram.com/atmosskateleague/reel/DU3hjmfApa4/',
+    username: 'atmosskateleague',
     videoUrl: '/videos/compressed/DU3hjmfApa4_min.mp4',
     posterUrl:
-      'https://scontent-iad3-1.cdninstagram.com/v/t51.82787-15/628935731_18091887830073123_51159256650512468_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=108&ccb=7-5&_nc_sid=18de74&oh=00_AftJhf-5KT697r6jaL0lVbIdNBldq5ZNAh_bYBiViac9Qw&oe=69A404B7',
+      'https://instagram.fdxb2-1.fna.fbcdn.net/v/t51.82787-15/628935731_18091887830073123_51159256650512468_n.jpg?stp=dst-jpg_e15_tt6&_nc_cat=108&ig_cache_key=MzgzNDY4MTE4OTc2ODI3MTU0NDE4MDkxODg3ODI0MDczMTIz.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjcyMHgxMjgwLnNkci5DMyJ9&_nc_ohc=La8pS7OV6mEQ7kNvwEyZCgo&_nc_oc=AdoxIoXtoRS1Zej9xbqozoG2lhYqRja6HZHlsPbb8Z_Er0uFlBQa8AMVpRz1kv-ULDo&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fdxb2-1.fna&_nc_gid=kb_C9FWq5HxRjtVr_s6EuQ&_nc_ss=7a32e&oh=00_Af2XIU45ky297vsiOPP6iiQ7_Wvd8wxYzbTkEK5ThfrNVg&oe=69DB2B77',
   },
   {
-    permalink: 'https://www.instagram.com/atmos.gear/reel/DUxg5CLCM6Y/',
-    username: 'atmos.gear',
+    permalink: 'https://www.instagram.com/atmosskateleague/reel/DUxg5CLCM6Y/',
+    username: 'atmosskateleague',
     videoUrl: '/videos/compressed/DUxg5CLCM6Y_min.mp4',
     posterUrl:
-      'https://scontent-iad3-1.cdninstagram.com/v/t51.82787-15/632076856_18091658855073123_5449618143099010301_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=108&ccb=7-5&_nc_sid=18de74&oh=00_AftuHTGWLYS_KI2z-tXgL4orTmHlI4ciLiF-oiUB-XGUqQ&oe=69A415DC',
+      'https://instagram.fdxb2-1.fna.fbcdn.net/v/t51.82787-15/632076856_18091658855073123_5449618143099010301_n.jpg?stp=dst-jpg_e15_tt6&_nc_cat=108&ig_cache_key=MzgzMjk4OTQxNDcwMDE0MjIzMjE4MDkxNjU4ODUyMDczMTIz.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjcyMHgxMjgwLnNkci5DMyJ9&_nc_ohc=p7UtF0yqQ-EQ7kNvwG5A3SP&_nc_oc=Adrhpb_nINuj-1gI916EkQLTqAHn2FnaOuWYGL92or9jJM3xBZ5IspqB6arPvKacISg&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fdxb2-1.fna&_nc_gid=kb_C9FWq5HxRjtVr_s6EuQ&_nc_ss=7a32e&oh=00_Af0Fzawgu1b-vc3nfAxTrXs7Z8s3XmLywb2QaOnj74ZWew&oe=69DB3C9C',
   },
 ] as const;
 
